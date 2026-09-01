bffe251c565a94fe769aef6e8786ccf006a8050c
Expert <alexej.baganov@yandex.ru>
2026-09-01 22:10:54 +0300

    Add user email and password hash

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -20,6 +20,15 @@ class User(Base):
         unique=True,
         nullable=False
     )
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+    password_hash: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
 
     created_at: Mapped[datetime] = mapped_column(
         DateTime,
